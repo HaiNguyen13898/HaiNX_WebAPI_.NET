0b5b529f25f3e6c88c8a9c8dc6a2d7f638f52bdc
HaiNguyen13898 <93204872+HaiNguyen13898@users.noreply.github.com>
2022-09-07 16:29:22 +0700

    up project

---
+++ WebAPI2/WebAPI2/Services/StudentService.cs
@@ -0,0 +1,49 @@
+﻿using WebAPI2.Models;
+
+namespace WebAPI2.Services
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IDbService _dbService;
+
+        public StudentService(IDbService dbService)
+        {
+            _dbService = dbService;
+        }   
+
+        public async Task<bool> CreateStudent(Student student)
+        {
+            var result = await _dbService.EditData(
+                "INSERT INTO student (id,name, age, address) VALUES (@Id, @Name, @Age, @Address)",
+                student);
+            return true;
+        }
+
+        public async Task<bool> DeleteStudent(int id)
+        {
+            var deleteStudent = await _dbService.EditData("DELETE FROM student WHERE id=@Id", new { id });
+            return true;
+        }
+
+        public async Task<List<Student>> GetStudentList()
+        {
+            var studentList = await _dbService.GetAll<Student>("SELECT * FROM student", new { });
+            return studentList;
+
+        }
+
+        public async Task<Student> UpdateStudent(Student student)
+        {
+            var updateStudent = await _dbService.EditData(
+                "Update student SET name=@Name, age=@Age, address=@Address WHERE id=@Id", 
+                student);
+            return student;
+        }
+
+        public async Task<Student> GetStudent(int id)
+        {
+            var studentList = await _dbService.GetAsync<Student>("SELECT * FROM student where id=@id", new { id });
+            return studentList;
+        }
+    }
+}
